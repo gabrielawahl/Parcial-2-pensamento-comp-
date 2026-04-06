bf39379cad5b60dd204056ca6db4de12b262fa1c
gabriela wahl <gabriela.wahl@escola.pr.gov.br>
2026-04-06 16:11:51 -0300

    Delete desafio 5.py

--- desafio 5.py
+++
@@ -1,8 +0,0 @@
-print("Bom dia!")
-
-base = float(input("Digite a base do triângulo: "))
-altura = float(input("Digite a altura do triângulo: "))
-
-area = (base * altura) / 2
-
-print(f"A área do triângulo é: {area}")
